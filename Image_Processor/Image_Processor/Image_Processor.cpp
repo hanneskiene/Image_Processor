620656bfb1d2ee3ca0c8a2659287f132a2f87212
hanneskiene <hannes-kiene@live.de>
2016-02-12 17:13:40 +0100

    Created Project

---
+++ Image_Processor/Image_Processor/Image_Processor.cpp
@@ -0,0 +1,12 @@
+#include "Image_Processor.h"
+
+
+
+Image_Processor::Image_Processor()
+{
+}
+
+
+Image_Processor::~Image_Processor()
+{
+}
---
+++ Image_Processor/Image_Processor/Image_Processor.h
@@ -0,0 +1,8 @@
+#pragma once
+class Image_Processor
+{
+public:
+	Image_Processor();
+	~Image_Processor();
+};
+
