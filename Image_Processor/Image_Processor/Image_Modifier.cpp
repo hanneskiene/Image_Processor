73cd591dc23a22a1e362e2cb89ebc6b79f6f1071
juli9797 <julian.dangelmaier@gmail.com>
2016-02-14 00:24:31 +0100

    fixed greyvalues

--- Image_Processor/Image_Processor/Image_Modifier.cpp
+++ Image_Processor/Image_Processor/Image_Modifier.cpp
@@ -10,14 +10,14 @@ void Image_Modifier::invert(Image_8 * image)
 {
 	auto pixels = image->get_pixels();
 	for (int i = 0; i < pixels->size(); i++) {
-		pixels->at(i)->set_RGB((255 - pixels->at(i)->get_b()), (255 - pixels->at(i)->get_g()), (255 - pixels->at(i)->get_b()));
+		pixels->at(i)->set_RGB((255 - pixels->at(i)->get_r()), (255 - pixels->at(i)->get_g()), (255 - pixels->at(i)->get_b()));
 	}
 }
 void Image_Modifier::greyValue(Image_8 * image)
 {
 	auto pixels = image->get_pixels();
 	for (int i = 0; i < pixels->size(); i++) {
-		unsigned char grauwert = (unsigned char)((pixels->at(i)->get_r()*0,299)+ (pixels->at(i)->get_g() * 0,587)+ (pixels->at(i)->get_b() * 0,114));
+		unsigned char grauwert = (unsigned char)(((pixels->at(i)->get_r())+ (pixels->at(i)->get_g())+ (pixels->at(i)->get_b()))/3);
 		pixels->at(i)->set_RGB(grauwert, grauwert, grauwert);
 	}
 }
